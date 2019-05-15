2729118d0bf047caefe3d6fcbea1278ecb3d881c
Andrew Martin <andy123@stanford.edu>
2019-05-15 09:51:15 -0700

    update logger

--- emLoggerTrait.php
+++ emLoggerTrait.php
@@ -2,43 +2,6 @@
 namespace Stanford\AdminNinja;
 /** @var \Stanford\AdminNinja\AdminNinja $this */
 
-
-/**
- * This trait can be use with External Modules to enable the Stanford emLogger module
- *
- * To use this on your project:
- * 1) simply add this trait to your EM project root
- * 2) change the NAMESPACE of this file to match that of your EM
- * 3) Before you declare your EM class, insert the command:  include "emLoggerTrait.php";
- * 4) Inside your class, before your first function, insert: use emLoggerTrait;
- * 5) (optional) Modify your config.json to include these two optional debug settings
-
-  (OPTIONAL) INSERT THESE OPTIONS INTO THE CONFIG.JSON
-
-  "system-settings": [
-
-    {
-      "key": "enable-system-debug-logging",
-      "name": "<b>Enable Debug Logging (system-wide)</b>",
-      "required": false,
-      "type": "checkbox"
-    },
-
-  ],
-
-  "project-settings": [
-
-    {
-      "key": "enable-project-debug-logging",
-      "name": "<b>Enable Debug Logging</b>",
-      "required": false,
-      "type": "checkbox"
-    },
-
-   ],
-
- */
-
 /**
  * This trait can be used with External Modules to enable the Stanford emLogger module
  *
@@ -82,43 +45,6 @@ trait emLoggerTrait
     private $emLoggerEnabled = null;    // Cache logger enabled
     private $emLoggerDebug   = null;    // Cache debug mode
 
-<<<<<<< HEAD
-    // Determine if emLogger is enabled on this server
-    private $emLoggerEnabled    = null;
-    private $emLoggerDebugMode  = null;
-
-    /**
-     * Obtain an instance of emLogger or false if not installed / active
-     * @return bool|mixed
-     */
-    function emLoggerInstance() {
-
-        // This is the first time used, see if it is enabled on server
-        if (is_null($this->emLoggerEnabled)) {
-            $versions = \ExternalModules\ExternalModules::getEnabledModules();
-            $this->emLoggerEnabled = isset($versions['em_logger']);
-        }
-
-        // Return instance if enabled
-        if ($this->emLoggerEnabled) {
-
-            // Set if debug mode once on the first log call
-            if (is_null($this->emLoggerDebugMode)) {
-                $this->emLoggerDebugMode = $this->emLoggerGetDebugMode();
-            }
-
-            // Try to return the instance of emLogger (which is cached by the EM framework)
-            try {
-                return \ExternalModules\ExternalModules::getModuleInstance('em_logger');
-            } catch (\Exception $e) {
-                // Unable to initialize em_logger
-                error_log("Exception caught - unable to initialize emLogger in " . __NAMESPACE__ . "/" . __FILE__ . "!");
-                $this->emLoggerEnabled = false;
-                return false;
-            }
-        } else {
-            return false;
-=======
 
     /**
      * Obtain an instance of emLogger or false if not installed / active
@@ -157,7 +83,6 @@ trait emLoggerTrait
             $systemDebug         = $this->getSystemSetting('enable-system-debug-logging');
             $projectDebug        = !empty($_GET['pid']) && $this->getProjectSetting('enable-project-debug-logging');
             $this->emLoggerDebug = $systemDebug || $projectDebug;
->>>>>>> 176022c2f3a98682b4f650b175745a2ab85aa38f
         }
         return $this->emLoggerDebug;
     }
@@ -173,29 +98,6 @@ trait emLoggerTrait
 
 
     /**
-<<<<<<< HEAD
-     * Determine if we are in debug mode either on system or project level
-     * @return bool
-     */
-    function emLoggerGetDebugMode() {
-        $systemDebug  = $this->getSystemSetting('enable-system-debug-logging');
-        $projectDebug = !empty($_GET['pid']) && $this->getProjectSetting('enable-project-debug-logging');
-        return $systemDebug || $projectDebug;
-    }
-
-
-    /**
-     * Do the logging
-     * The reason we broke it into three functions was to reduce complexity with backtrace and the calling function
-     */
-    function emLog() {
-        if ($emLogger = $this->emLoggerInstance()) $emLogger->emLog($this->PREFIX, func_get_args(), "INFO");
-    }
-
-
-    /**
-=======
->>>>>>> 176022c2f3a98682b4f650b175745a2ab85aa38f
      * Wrapper for logging an error
      */
     function emError() {
@@ -207,11 +109,7 @@ trait emLoggerTrait
      * Wrapper for logging debug statements
      */
     function emDebug() {
-<<<<<<< HEAD
-       if ( $this->emLoggerDebugMode && ($emLogger = $this->emLoggerInstance()) ) $emLogger->emLog($this->PREFIX, func_get_args(), "DEBUG");
-=======
         if ( $this->emLoggerDebugMode() && ($emLogger = $this->emLoggerInstance()) ) $emLogger->emLog($this->PREFIX, func_get_args(), "DEBUG");
->>>>>>> 176022c2f3a98682b4f650b175745a2ab85aa38f
     }
 
 }
